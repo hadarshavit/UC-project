1e4b578a560d7f65efab8fcad9ec1b6ffa559248
Hadar Shavit <shavit.hadar@gmail.com>
2021-12-29 22:32:22 +0100

    sleep|

--- run_artificial.sh
+++ run_artificial.sh
@@ -1,8 +1,15 @@
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data True --seed 10 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data True --seed 20 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data True --seed 30 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data True --seed 40 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data True --seed 50 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data True --seed 60 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data True --seed 70 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data True --seed 80 &
--- run_real.sh
+++ run_real.sh
@@ -1,8 +1,15 @@
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data False --seed 1 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data False --seed 2 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data False --seed 3 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data False --seed 4 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data False --seed 5 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data False --seed 6 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data False --seed 7 &
+sleep 10
 nohup python CityTransfer\Main.py --autoencoder Default --add_artificial_data False --seed 8 &
